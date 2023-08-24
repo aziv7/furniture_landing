4b74a1693f4adfed413d344e4e4783f477ebd0ae
aziv7 <aziz.jerbi@esprit.tn>
2023-08-24 15:50:25 +0100

    Learn more section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './sections/Header';
 import Hero from './sections/Hero';
 import Services from './sections/Services';
 import Inspiration from './sections/Inspiration';
+import LearnMore from './sections/LearnMore';
 function App() {
   return (
     <div className='lg:max-w-7xl m-0 mx-auto min-h-screen'>
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Services />
       <Inspiration />
+      <LearnMore />
     </div>
   );
 }
