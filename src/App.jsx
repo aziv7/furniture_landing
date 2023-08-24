20138f0f2a630844c4999e14d84c7921dff22e9a
aziv7 <aziz.jerbi@esprit.tn>
2023-08-24 13:14:02 +0100

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './sections/Header';
+import Hero from './sections/Hero';
 
 function App() {
   return (
     <div className='lg:max-w-7xl m-0 mx-auto'>
       <Header />
+      <Hero />
     </div>
   );
 }
